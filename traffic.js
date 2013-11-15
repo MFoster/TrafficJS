fe9c04954a57da38c8abf06e246a3fcd1e0fb884
matt foster <mattfoster01@gmail.com>
2013-11-15 10:40:55 -0500

    Extracted the main traffic file into it's own file

--- traffic.js
+++ traffic.js
@@ -35,18 +35,7 @@ var sync = function(method, model, options){
   return this.request.send();
 }
 
-/**
- * Generic class that simply calls it's own initialize method
- */
-var BaseClass = function(){
-  this.initialize.apply(this, arguments);
-};
 
-//hijack's Backbone's extend method
-BaseClass.extend = Backbone.Model.extend;
-
-//Assigns a named function to the initialize method.  
-BaseClass.prototype.initialize = function BaseClassConstructor(){};
 
 /**
  * Creates a separate reference
